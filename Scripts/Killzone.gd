52c0fda30606f994a3739ddb7a6a3babc06d05ca
KeichiTS <kkeichi@gmail.co>
2022-01-28 02:09:02 -0300

    levels

--- Scripts/Killzone.gd
+++ Scripts/Killzone.gd
@@ -1,10 +1,11 @@
-extends Area2D
+extends StaticBody2D
 
 
 func _ready():
 	pass
 
 
-func _on_Killzone_body_entered(body):
+func _on_killzone_body_entered(body):
 	if body.name == "Player":
+		body.timer_status()
 		body.reset()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,8 +10,11 @@ export var speed = 12000
 export var inertia = 100
 export var energy = 100
 
-enum{to_be,not_to_be}
+enum{to_be,not_to_be,transformation}
 var status = to_be 
+
+enum{on_timer,out_timer}
+var timer_status = out_timer
 
 func _ready():
 	pass
@@ -40,32 +43,74 @@ func move(val):
 
 	if input_vector.y > 0 and input_vector.x == 0:
 		if status == to_be: 
-			$Sprite.frame = 0
+			#$Sprite.frame = 0
+			$Be_sprites/idle.hide()
+			$Be_sprites/front.show()
+			$Be_sprites/back.hide()
+			$Be_sprites/side.hide()
+			
 		else:
-			$Sprite.frame = 1
+			$Not_be_sprites/idle.hide()
+			$Not_be_sprites/front.show()
+			$Not_be_sprites/back.hide()
+			$Not_be_sprites/side.hide()
 	
 	elif input_vector.y < 0 and input_vector.x == 0:
 		if status == to_be:
-			$Sprite.frame = 0 
+			$Be_sprites/idle.hide()
+			$Be_sprites/front.hide()
+			$Be_sprites/back.show()
+			$Be_sprites/side.hide()
 		else:
-			$Sprite.frame = 1
+			$Not_be_sprites/idle.hide()
+			$Not_be_sprites/front.hide()
+			$Not_be_sprites/back.show()
+			$Not_be_sprites/side.hide()
 	
 	elif input_vector.x > 0:
-		$Sprite.scale.x = -1
+		#$Sprite.scale.x = -1
+		$Be_sprites/side.scale.x = -1
+		$Not_be_sprites/side.scale.x = -1
 		if status == to_be:
-			$Sprite.frame = 2
+			$Be_sprites/idle.hide()
+			$Be_sprites/front.hide()
+			$Be_sprites/back.hide()
+			$Be_sprites/side.show()
 		else:
-			$Sprite.frame = 3 
+			$Not_be_sprites/idle.hide()
+			$Not_be_sprites/front.hide()
+			$Not_be_sprites/back.hide()
+			$Not_be_sprites/side.show()
 			
 	elif input_vector.x < 0:
-		$Sprite.scale.x = 1
+		#$Sprite.scale.x = 1
+		$Be_sprites/side.scale.x = 1
+		$Not_be_sprites/side.scale.x = 1
 		if status == to_be:
-			$Sprite.frame = 2 
+			$Be_sprites/idle.hide()
+			$Be_sprites/front.hide()
+			$Be_sprites/back.hide()
+			$Be_sprites/side.show()
 		else:
-			$Sprite.frame = 3
+			$Not_be_sprites/idle.hide()
+			$Not_be_sprites/front.hide()
+			$Not_be_sprites/back.hide()
+			$Not_be_sprites/side.show()
 			
-
-	move_and_slide(velocity,Vector2(0,0),false, 4, 0, false)
+	else:
+		if status == to_be:
+			$Be_sprites/idle.show()
+			$Be_sprites/front.hide()
+			$Be_sprites/back.hide()
+			$Be_sprites/side.hide()
+		else:
+			$Not_be_sprites/idle.show()
+			$Not_be_sprites/front.hide()
+			$Not_be_sprites/back.hide()
+			$Not_be_sprites/side.hide()
+			
+	if status != transformation:
+		move_and_slide(velocity,Vector2(0,0),false, 4, 0, false)
 
 
 
@@ -85,25 +130,44 @@ func input_reset():
 
 ### Faz o bixo resetar conforme um parâmetro
 func reset():
-	get_tree().reload_current_scene()
+	if timer_status == out_timer:
+		status = transformation
+		$Be_sprites.hide()
+		$Not_be_sprites.hide()
+		$Transitions/anim.play("Dead")
+		yield($Transitions/anim,"animation_finished")
+		get_tree().reload_current_scene()
 
 ### Faz o bixo ser ou não ser
 func to_be():
+	
 	if Input.is_action_just_pressed("ui_change"):
 		if status == to_be and energy > 0:
-			$ColorRect.color.a = .5
+			timer_status = on_timer
 			collision_layer = 2
+			$Be_sprites.hide()
+			status = transformation
+			$Transitions/anim.play("Transformation")
+			yield($Transitions/anim,"animation_finished")
+			$Not_be_sprites.show()
 			status = not_to_be
 			$energy_timer.start()
 			
-		else:
-			$ColorRect.color.a = 1
-			collision_layer = 1
-			status = to_be
-			$energy_timer.stop()
+#		elif status == not_to_be and energy >= 0:
+#			$ColorRect.color.a = 1
+#			collision_layer = 1
+#			$Not_be_sprites.hide()
+#			$Transitions/anim.play("Detransformation")
+#			yield($Transitions/anim,"animation_finished")
+#			$Be_sprites.show()
+#			status = to_be
+#			$energy_timer.stop()
 	
 
 ###
+
+func timer_status():
+	timer_status = out_timer
 
 ###################################################
 #     ~ It ain't much, but it's honest work ~     #
@@ -134,10 +198,18 @@ func to_be():
 func _on_energy_timer_timeout():
 	if energy > 0:
 		energy -= 10
-	elif energy <= 0:
+	elif energy <= 0 and timer_status == on_timer:
+		timer_status = out_timer
 		energy = 0
-		status = to_be
-		$ColorRect.color.a = 1
+		status = transformation
 		collision_layer = 1
+		$Not_be_sprites.hide()
+		$Transitions/anim.play("Detransformation")
+		yield($Transitions/anim,"animation_finished")
+		status = to_be
+		$Be_sprites.show()
+		$energy_timer.stop()
+		
+		
 		
 	$Energy.text = str(energy)
