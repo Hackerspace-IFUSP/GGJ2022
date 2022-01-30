188c0c3170d25ba10e922ce73b8ba4ebc1e3abfa
jaspior <ricardo.pitta@usp.br>
2022-01-30 01:03:21 -0300

    steps and stones

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -9,8 +9,10 @@ func disable_collision():
 	collision_mask = 8
 	collision_layer = 8
 	#$shape.disabled = true
+	$Sprite.play("Open")
 	
 func enable_collision():
 	#$shape.disabled = false
 	collision_mask = 1
 	collision_layer = 1
+	$Sprite.play("Close")
